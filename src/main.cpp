38ca0b1911d786682d798e056e220ea0972b7238
Johan Medrano <jmsnowrider@gmail.com>
2018-01-11 14:59:24 +0100

    Code fonctionnel pour les servos, testé à l'analyseur logique

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 uint8_t id = 0x03; 
 Serial pc(USBTX, USBRX, 9600); 
 
-Serial connection(D8,D2);
+Serial connection(D8,D2, 115200);
 
 int main() 
 {  
@@ -21,36 +21,36 @@ int main()
     
     sv.getStatus(0x03);
 
-    while (1) {
-	    sleep();
-    }
-    /*
+    // while (1) {
+	   //  sleep();
+    // }
+    
     sv.getPos(0x03);
    
     while(1) 
     {   
         // POS Mode, +159.8 Degree, 100*11.2ms = 1120ms, Green LED On
         sv.positionControl(0x03, 1002, 100, GLED_ON);
-        wait(5);
+        wait_us(5);
         
         // POS Mode, -159.8 Degree, 100*11.2ms = 1120ms, Blue LED On
         sv.positionControl(0x03, 21, 100, BLED_ON);
-        wait(5);
+        wait_us(5);
  
         // TURN Mode, CCW Turn, Green LED On
         sv.velocityControl(0x03, 1023, GLED_ON);
-        wait(5);
+        wait_us(5);
         
         // TURN Mode, CW Turn, Blue LED On        
         sv.velocityControl(0x03, -1023, BLED_ON);
-        wait(5);
+        wait_us(5);
         
         // POS Mode, 0 Degree, 100*11.2ms = 1120ms, Red LED On
         sv.positionControl(0x03, 512, 100, RLED_ON);
-        wait(5);
+        wait_us(5);
         
         // Clear error status
         sv.clear(0x03);
     }
-    */
+    
 }
