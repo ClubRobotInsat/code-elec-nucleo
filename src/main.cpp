99d28e1cd5ef1ea46a1e351b5623bac34ff4a423
Paul Florence <florencepaul@hotmail.fr>
2018-01-15 14:07:00 +0100

    Nouvelle lib pour la gestion des servos.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "herkulex.h"
+#include "bus.h"
 #include "mbed.h"
 
 // set serial port and baudrate, (mbed <-> HerculexX)
@@ -9,35 +9,17 @@ Serial connection(D8, D2, 115200);
 
 int main() {
 	pc.printf("xxxXXX-DEBUT-XXXxxx");
-	pc.printf("lol\n");
 
-	Herkulex sv3(0x03, &connection, &pc);
-	// Herkulex svfd(0xfd, &connection, &pc);
-
-	pc.printf("lol ?\n");
-
+	HerkulexBus bus(&connection);
+	HerkulexServo sv3 = bus.makeNewServo(0x03);
 	sv3.clear();
-	sv3.setTorque(TORQUE_ON);
-
 	sv3.setLedOn();
+	sv3.setTorque(TORQUE_ON);
 	sv3.positionControl(800, 60, GLED_ON);
-	wait_ms(800);
-	sv3.positionControl(300, 60, GLED_ON);
-	sv3.fetchStatus();
-
-	// svfd.clear();
-	// svfd.setTorque(TORQUE_ON);
+	sv3.updateStatus();
+	sv3.updatePosition();
 
 	while(true) {
-		// sv.setLedOn(0x03);
-		// wait_us(500);
-
-		// sv3.positionControl(800, 60, GLED_ON);
-		// svfd.positionControl(300, 60, BLED_ON);
-		// wait_ms(7);
-
-		// sv3.positionControl(300, 60, GLED_ON);
-		// svfd.positionControl(800, 60, BLED_ON);
-		// wait_ms(700);
+		wait_ms(7);
 	}
 }
