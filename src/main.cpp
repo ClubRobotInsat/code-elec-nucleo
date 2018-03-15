9a60bd570819d5188060a33cfd42546a20376838
Paul Meis <meis@etud.insa-toulouse.fr>
2018-03-15 18:52:09 +0100

    Ajout fonctions de traitement de trame Servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "HerkulexBus.h"
 #include "HerkulexConst.h"
 #include "HerkulexManager.h"
+#include "HerkulexServo.h"
 #include "mbed.h"
 
 #include <trame.h>
@@ -12,6 +13,28 @@ uint8_t id = 0xFD;
 Serial pc(USBTX, USBRX, 9600);
 
 CircularBuffer<Trame, 256> file_attente;
+
+herkulex::Manager<6> servo_manager (D8,D2,500,&pc);
+
+void traiterTrameServo(Trame trame_servo);
+
+// Forme un uint16_t avec 2 uint8_t
+uint16_t paquetage(uint8_t msb,uint8_t lsb)
+{
+	uint16_t resultat = 0x0000;
+	resultat = msb;
+	resultat << 8;
+	resultat |= lsb;
+	return resultat;
+}
+
+void init_servo()
+{
+	Servo* servo_fd = servo_manager.registerNewServo(0xFD);
+	servo_fd->reboot();
+	Servo* servo_03 = servo_manager.registerNewServo(0x03);
+	servo_03->reboot();
+}
 
 Trame lire_trame(Serial* pc) {
 	uint8_t numPaquet, id, cmd, data_length;
@@ -30,7 +53,7 @@ Trame lire_trame(Serial* pc) {
 						data_length = pc->getc();
 						data = new uint8_t[data_length];
 						for(int j = 0; j < data_length; j++) {
-							*data = pc->getc();
+							data[j] = pc->getc();
 						}
 						done = true;
 					}
@@ -61,10 +84,88 @@ void afficherTrame() {
 	}
 }
 
+void traiterTrame(){
+	Trame trame_traitee;
+	uint8_t id;
+	// Si une trame est dans le buffer on la recupere
+	if(file_attente.pop(trame_traitee))
+	{
+		id = trame_traitee.getId();
+		if(id == 1)
+		{
+			// traiter can
+		}
+		else if(id == 2)
+		{
+			traiterTrameServo(trame_traitee);
+		}
+		else
+		{
+			// erreur
+		}
+	}
+}
+
+void traiterTrameServo(Trame trame_servo)
+{
+	uint8_t id_servo = 0;
+	uint16_t angle = 0;
+	
+	// longueur generique d'une trame servo
+	uint8_t longueur_trame_servo = 3;
+
+	herkulex::Servo * servo_commande = NULL;
+
+	// valeurs recuperees dans la trame
+	uint8_t angle_lsb = 0;
+	uint8_t angle_msb = 0;
+
+	// Si la trame fait la bonne taille (3 octets)
+	if(trame_servo.getDataLength() == longueur_trame_servo)
+	{
+		switch(trame_servo.getCmd())
+		{
+		case 0x05 :
+		// 1er octet = id
+		// octets 2 et 3 = angle (position)
+
+		// Recuperer l'id de la trame
+		id_servo = (trame_servo.getData())[0];
+
+		// Concatener les 2 octets du champs data de la trame
+		angle_msb = (trame_servo.getData())[1];
+		angle_lsb = (trame_servo.getData())[2];
+		angle = paquetage(angle_msb,angle_lsb);
+
+		// Faire tourner le servo concerne
+		if((servo_commande = servo_manager.getServoById(id_servo)) != NULL)
+		{
+			servo_commande->setPosition(angle);
+		}
+		else
+		{
+			debug("idServo non trouve\n");
+		}
+		break;
+
+		default:
+		debug("Erreur commande trame\n");
+		break;
+		}
+	}
+	else
+	{
+		debug("Erreur longueur de trame\n");
+	}
+	
+}
+
 int main() {
-	mettreTrameDansFileAttente();
+	init_servo();
 	Ticker ticker;
-	ticker.attach(afficherTrame, 0.005);
-	while(true) {
+	ticker.attach(traiterTrame, 0.005);
+	while(true) 
+	{
+		mettreTrameDansFileAttente();
 	}
 }
