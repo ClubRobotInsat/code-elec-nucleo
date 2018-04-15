2897340d078d53aa1c0123f2aba1d83c7791c110
Paul Meis <meis@etud.insa-toulouse.fr>
2018-04-15 19:53:49 +0200

    Ajout du PID

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "QEI.h"
 
 #define PPR 4096
+#define MIN_PWM 0.08
+#define MARGE 150
 
 Serial pc(USBTX, USBRX);
 
@@ -14,12 +16,12 @@ class Motor
 			, _motor_speed_control(pin_motor_control)
 			, _motor_direction_control(pin_direction_control)
 			, _prev_error(0.0)
+			, _error_sum(0.0)
 			  {
 				_motor_speed_control.period(0.0001);
 				_motor_speed_control.write(0);
 				_motor_direction_control.write(0);
 			}
-
 
 		/* L'angle est en degré entre 0 et 360 !!!! */
 		void set_position(float angle) {
@@ -29,62 +31,114 @@ class Motor
 			_pulse_wanted = static_cast<int>((pulses/360.0)*angle);
 		}
 
+		static void afficher_coeff() {
+			printf("Kp : %f | Ki : %f | Kd : %f\n\r",Kp*10000,Ki*10000,Kd*10000);
+		}
 
 		void asserv() {
 
 			int pulses = _encoder.getPulses();
-			if(_pulse_wanted != pulses) {
+
+			if(_pulse_wanted >= pulses+MARGE || _pulse_wanted <= pulses-MARGE) {
 
 				float error = abs(pulses - _pulse_wanted);
 				float delta_error = fabs(error - _prev_error);
+				_error_sum += error;
 				_prev_error = error;
 
-				float pwm_value = static_cast<float>(error * Kp)/10000.0 + static_cast<float>(delta_error*Kd)/10000.0;
-				debug("Je veut être en %d et je suis en %d => déplacement avec pour consigne %f\n\r",_pulse_wanted,pulses,pwm_value);
+
+				// /!\ PWM >= MIN_PWM sinon le moteur ne tourne pas
+				float pwm_value = static_cast<float>(error * Kp) + static_cast<float>(delta_error*Kd) + static_cast<float>(_error_sum*Ki);
+
+				if(pwm_value < MIN_PWM)
+				{
+					pwm_value = MIN_PWM;
+				}
+
+				//debug("Je veut être en %d et je suis en %d => déplacement avec pour consigne %f\n\r",_pulse_wanted,pulses,pwm_value);
+
 				if (_pulse_wanted < pulses) {
-					debug("Je recule ! \n\r");
 					_motor_speed_control.write(pwm_value);
 					_motor_direction_control.write(1);
 				} else {
-					debug("J'avance  ! \n\r");
 					_motor_speed_control.write(pwm_value);
 					_motor_direction_control.write(0);
 				}
+
+
+				
 			}
 			else {
-				debug("Je suis en position ! \n\r");
+				//debug("Je suis en position ! \n\r");
 				_motor_speed_control.write(0);
+				_error_sum = 0;
+				_prev_error = 0;
 			}
 		}
 
+		static float Kp;
+		static float Ki;
+		static float Kd;
+
 	private :
-		constexpr static float Kp = 0.125;
-		constexpr static float Kd = 0.08;
+
+
 		int32_t _pulse_wanted;
 		QEI _encoder;
 		PwmOut _motor_speed_control;
 		DigitalOut _motor_direction_control;
 		float _prev_error;
+		float _error_sum;
 
 };
+
+
+float Motor::Kp = 0.157/10000;
+float Motor::Ki = 0.006471/10000;
+float Motor::Kd = 0.005000/10000;
+
 
 int i = 0;
 
 Motor motor(D3,D5,D9,D10,0.04);
+void update_pid();
+
 void avancer() {
 	//motor.set_position( ((float) (i % 4)) * 90.0);q7
 	motor.set_position( ((float) (i % 2)) * 180.0);
 	i++;
 }
 
+AnalogIn akp(A0);
+AnalogIn aki(A1);
+AnalogIn akd(A2);
+
+float max_kp = 0.4/10000;
+float max_ki = 0.1/10000;
+float max_kd = 0.2/1000;
+
+void update_pid() {
+
+	float kp = akp.read() * max_kp;
+	float ki = aki.read() * max_ki;
+	float kd = akd.read() * max_kd;
+
+	Motor::Kp = kp;
+	Motor::Ki = ki;
+	Motor::Kd = kd;
+
+	//Motor::afficher_coeff();
+}
+
 int main() {
 
 	Ticker av;
-	av.attach(&avancer,3.0);
+	//av.attach(&avancer,4.0);
 
 	while(1) {
 		wait_ms(1);
 		motor.asserv();
+		//update_pid();
 	}
 
 }
