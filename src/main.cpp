bdb3ab849f891b5e3df08e634508bac493cc8ba5
dotsumae <alex@gr00t.home>
2018-04-16 18:22:28 +0200

    Fonction de controle et de test d'un ESC. Elle utilise un lib Servo.h facile à importer

--- src/main.cpp
+++ src/main.cpp
@@ -1,143 +1,26 @@
-#include "HerkulexBus.h"
-#include "HerkulexConst.h"
-#include "HerkulexManager.h"
-#include "HerkulexServo.h"
 #include "mbed.h"
+#include "Servo.h"
+#define PIN_ESC D9
 
-#include <trame.h>
 
-using namespace herkulex;
-// set serial port and baudrate, (mbed <-> HerculexX)
-uint8_t id = 0xFD;
+Servo moteur(PIN_ESC);
+AnalogIn bouton(A0);
 
-Serial pc(USBTX, USBRX, 9600);
-Serial logger(D8, D2, 9600);
-
-herkulex::Manager<6> servo_manager(A0, A1, 2, &pc);
-
-void traiterTrameServo(Trame trame_servo);
-
-// Forme un uint16_t avec 2 uint8_t
-uint16_t paquetage(uint8_t msb, uint8_t lsb) {
-	uint16_t resultat = 0x0000;
-	resultat = msb;
-	resultat = resultat << 8;
-	resultat |= lsb;
-	return resultat;
+int controleVentilateur(int entree, Servo * moteurCible) //entree = 0 => moteur éteint, allume sinon
+{
+    (entree == 0) ? (*moteurCible = 1.0) : (*moteurCible = 0.0);
+    return 0;
 }
 
-void init_servo() {
-	pc.printf("Initialisation des servos\n\r");
-	Servo* servo_fd = servo_manager.registerNewServo(0xFD);
-	servo_fd->setPosition(512);
-	servo_fd->reboot();
-	Servo* servo_03 = servo_manager.registerNewServo(0x03);
-	servo_03->setPosition(512);
-	servo_03->reboot();
+int main()
+{
+    moteur = 0.0;
+    wait(2);
+    
+    while(1)
+    {
+    (bouton == 0) ? (controleVentilateur(0, &moteur)) : (controleVentilateur(1, &moteur));
+    }
+    
 }
-
-Trame lire_trame(Serial* pc) {
-	uint8_t numPaquet, id, cmd, data_length;
-	uint8_t* data;
-	bool done = false;
-	while(not done) {
-		if(pc->getc() == 0xAC) {
-			if(pc->getc() == 0xDC) {
-				if(pc->getc() == 0xAB) {
-					if(pc->getc() == 0xBA) {
-						numPaquet = pc->getc();
-						uint8_t aux1 = pc->getc();
-						uint8_t aux2 = pc->getc();
-						id = Trame::demultiplexId(aux1, aux2);
-						cmd = Trame::demultiplexCmd(aux1, aux2);
-						data_length = pc->getc();
-						data = new uint8_t[data_length];
-						for(int j = 0; j < data_length; j++) {
-							data[j] = pc->getc();
-						}
-						done = true;
-					}
-				}
-			}
-		}
-	}
-	//pc->write(Trame::makeAck(numPaquet), 15, NULL, 0);
-	return Trame(id, cmd, data_length, data);
-}
-
-void afficherTrame(Trame trame) {
-	pc.printf("Trame reçue : id %#x | cmd %#x | data_length %#x | data ", trame.getId(), trame.getCmd(), trame.getDataLength());
-	uint8_t* data = trame.getData();
-	for(int i = 0; i < trame.getDataLength(); i++) {
-		pc.printf("%#x ", data[i]);
-	}
-	pc.printf("\n\r");
-}
-
-void traiterTrame(Trame trame) {
-	uint8_t id;
-	// Si une trame est dans le buffer on la recupere
-	id = trame.getId();
-	if(id == 1) {
-		pc.printf("Trame can reçue\n\r");
-	} else if(id == 2) {
-		pc.printf("Trame servo reçue\n\r");
-		traiterTrameServo(trame);
-	} else {
-		pc.printf("Id de trame invalide\n\r");
-	}
-}
-
-void traiterTrameServo(Trame trame_servo) {
-	uint8_t id_servo = 0;
-	uint16_t angle = 0;
-
-	// longueur generique d'une trame servo
-	uint8_t longueur_trame_servo = 3;
-
-	// valeurs recuperees dans la trame
-	uint8_t angle_lsb = 0;
-	uint8_t angle_msb = 0;
-
-	// Si la trame fait la bonne taille (3 octets)
-	if(trame_servo.getDataLength() == longueur_trame_servo) {
-		switch(trame_servo.getCmd()) {
-			case 0x05: {
-				// 1er octet = id
-				// octets 2 et 3 = angle (position)
-
-				// Recuperer l'id de la trame
-				id_servo = (trame_servo.getData())[0];
-
-				// Concatener les 2 octets du champs data de la trame
-				angle_msb = (trame_servo.getData())[1];
-				angle_lsb = (trame_servo.getData())[2];
-				angle = paquetage(angle_msb, angle_lsb);
-
-				// Faire tourner le servo concerne
-				herkulex::Servo* servo_commande = servo_manager.getServoById(id_servo);
-				if(servo_commande != nullptr) {
-					pc.printf("Déplacement du servo %#x \n\r", id_servo);
-					servo_commande->setPosition(angle);
-				} else {
-					pc.printf("idServo non trouve\n\r");
-				}
-				break;
-			}
-
-			default:
-				pc.printf("Erreur commande trame\n\r");
-				break;
-		}
-	} else {
-		pc.printf("Erreur longueur de trame\n\r");
-	}
-}
-
-int main() {
-	init_servo();
-	while(true) {
-		Trame trame = lire_trame(&pc);
-		traiterTrame(trame);
-	}
-}
+    
