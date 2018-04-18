5347ece547212c05bbb46ac3d0bccf2ad0a8057c
Paul Florence <florencepaul@hotmail.fr>
2018-04-18 15:50:43 +0200

    Affichage des trames CAN reçues.

--- src/main.cpp
+++ src/main.cpp
@@ -3,41 +3,25 @@ Ticker ticker;
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 
-CAN can1(PA_11, PA_12,100000);
-//CAN can2(PB_5, PB_13,100000);
-char counter = 0;
-void send() {
-	printf("send()\n\r");
-	if(can1.write(CANMessage(1337, &counter, 1))) {
-		printf("wloop()\n\r");
-		counter++;
-		printf("Message sent: %d\n\r", counter);
-	} 
-	else {
-		printf("Erreur d'envoi : %d", can1.tderror());
+CAN can1(PA_11, PA_12,500000);
+
+void afficher_message(CANMessage mess) {
+	debug("CAN : %#x (%d) : ",mess.id,mess.len);
+	for (uint8_t i = 0; i < mess.len; i++) {
+		debug("%#x ",mess.data[i]);
 	}
-	led1 = !led1;
+	debug("\n\r");
 }
 
 int main() {
-	printf("main()---------------------------\n\r");
-	ticker.attach(&send, 1);
-//	CANMessage msg;
+	printf("Start \n\r");
+	CANMessage msg;
 	while(1) {
-/*		printf("loop()\n\r");
-		
 		if(can1.read(msg)) {
-			if (msg.len == 1) {
-			printf("Message received: %d\n\r", msg.data[0]);
-			}
-			else {
-				printf("slt : %d \n\r", msg.len);
-			}
-			led2 = !led2;
+			afficher_message(msg);
 		} else {
-			printf("Erreur de lecture : %d", can1.rderror());
+			printf("Erreur de lecture : %d\n\r", can1.rderror());
 		}
-		*/
 		wait(0.2);
 	}
 }
