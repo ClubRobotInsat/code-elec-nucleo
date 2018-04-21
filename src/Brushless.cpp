2bc0287f0c210a0be5de4562a9b7abab52b5f7fb
Paul Florence <florencepaul@hotmail.fr>
2018-04-21 11:13:01 +0200

    Rajout de la classe pour les moteurs brushless.

---
+++ src/Brushless.cpp
@@ -0,0 +1,16 @@
+#include "Brushless.h"
+
+Brushless::Brushless(PinName motor_pin) : _motor(motor_pin) {}
+
+void Brushless::set_brushless_state(BrushlessState state) {
+	switch(state) {
+		case ON: {
+			_motor = 1.0;
+			break;
+		}
+		case OFF: {
+			_motor = 0.0;
+			break;
+		}
+	}
+}
---
+++ src/Brushless.h
@@ -0,0 +1,21 @@
+#ifndef BRUSHLESS_C
+#define BRUSHLESS_C
+
+#include "Servo.h"
+#include "mbed.h"
+
+enum BrushlessState { ON, OFF };
+
+
+class Brushless {
+
+public:
+	Brushless(PinName motor_pin);
+
+	void set_brushless_state(BrushlessState state);
+
+private:
+	Servo _motor;
+};
+
+#endif
