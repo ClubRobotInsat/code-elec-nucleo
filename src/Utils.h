2358dfcd6846f94ecf79f1b5de8a0ae3d4c9c931
Paul Florence <florencepaul@hotmail.fr>
2018-04-24 11:13:37 +0200

    Rajout d'un fichier avec des fonctions utilitaires.

---
+++ src/Utils.h
@@ -0,0 +1,18 @@
+#ifndef UTILS_ROBOT_H
+#define UTILS_ROBOT_H
+
+#include <mbed.h>
+
+float make_float(uint8_t first, uint8_t second, uint8_t third, uint8_t last) {
+	uint32_t temp =  ((first << 24) | (second << 16) | (third << 8) | last);
+	return static_cast<float>(temp);
+}
+
+uint16_t make_u16(uint8_t msb, uint8_t lsb) {
+	return (msb >> 8) | lsb;
+}
+
+
+
+
+#endif
