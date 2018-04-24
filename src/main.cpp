1e7b858a8e9eed65cbb6e61a853e456f87916fb9
Paul Florence <florencepaul@hotmail.fr>
2018-04-24 17:59:16 +0200

    Rajout d'une ligne pour la simulation du code elec.

--- src/main.cpp
+++ src/main.cpp
@@ -137,7 +137,12 @@ int main() {
 #include "Robot.h"
 #include "mbed.h"
 
+
+#ifdef SIMULATED_MAIN
+int main_elec() {
+#else
 int main() {
+#endif
 	Robot robot;
 	robot.initialize_meca();
 	wait_ms(500);
