479fdc3b18dc46a40ff64678c8efe548be4d870a
Paul Florence <florencepaul@hotmail.fr>
2018-04-25 10:19:42 +0200

    Meilleur découpage des fonctions utilitaires.

--- src/Utils.h
+++ src/Utils.h
@@ -3,14 +3,9 @@
 
 #include <mbed.h>
 
-float make_float(uint8_t first, uint8_t second, uint8_t third, uint8_t last) {
-	uint32_t temp = ((first << 24) | (second << 16) | (third << 8) | last);
-	return static_cast<float>(temp);
-}
+float make_float(uint8_t first, uint8_t second, uint8_t third, uint8_t last);
 
-uint16_t make_u16(uint8_t msb, uint8_t lsb) {
-	return (msb >> 8) | lsb;
-}
+uint16_t make_u16(uint8_t msb, uint8_t lsb);
 
 
 #endif
